3e2bfa6cec2ab71c44e62b965c3e215bf70bd741
AirboZH <993372253@qq.com>
2021-03-05 01:14:21 +0800

    writ a websocket server

---
+++ lab/server.js
@@ -0,0 +1,11 @@
+const { Socket } = require('dgram')
+const ws = require('nodejs-websocket');
+
+const server = ws.createServer(socket=>{
+    socket.on('text',str=>{
+        console.log('str');
+        socket.sendText('收到消息了');
+    })
+}).listen(9999,()=>{
+    console.log('server on port 9999');
+});
