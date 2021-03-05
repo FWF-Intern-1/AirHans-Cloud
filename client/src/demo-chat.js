8ef7d3901b4d9e616fa095f131b427aed0cfa3ba
AirboZH <993372253@qq.com>
2021-03-05 21:53:52 +0800

    1.0

--- client/src/demo-chat.js
+++ client/src/demo-chat.js
@@ -1,5 +1,27 @@
 import { bubble } from './bubble.js'
 import { getDOM } from './getDOM.js'
+
+var ws = new WebSocket("ws://tomzhang.com.cn:9999");
+ws.onopen=()=>{
+    console.log("connected")
+}
+function sendmsg(){
+    ws.send(getDOM("typing"))
+}
+
+ws.onmessage = (evt)=>{
+    var received_msg = evt.data;
+    bubble(received_msg,i,true)
+}
+
+ws.onclose = ()=>{ 
+    alert("连接已关闭..."); 
+};
+
+
+bubble("111111",0,true);
+bubble("2222222",0,true);
+bubble("3333333",0,false);
 console.log(getDOM("typing"));
-bubble("123123",0,true);
-bubble("123123",0,false);
+
+export{sendmsg}
