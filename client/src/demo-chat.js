f50e968c62d342bc4a245a058a8b66c9c823c40f
AirboZH <993372253@qq.com>
2021-03-06 00:29:23 +0800

    chatting with yourself

--- client/src/demo-chat.js
+++ client/src/demo-chat.js
@@ -2,26 +2,24 @@ import { bubble } from './bubble.js'
 import { getDOM } from './getDOM.js'
 
 var ws = new WebSocket("ws://tomzhang.com.cn:9999");
+
 ws.onopen=()=>{
     console.log("connected")
 }
-function sendmsg(){
-    ws.send(getDOM("typing"))
-}
 
-ws.onmessage = (evt)=>{
+$("#chatBox--input--button__send").on("click",() => {
+    console.log("点击！");
+    ws.send(getDOM("typing").value);
+    bubble(getDOM("typing").value,1,false)
+    getDOM("typing").value=""
+})
+
+ws.onmessage = (evt) => {
     var received_msg = evt.data;
-    bubble(received_msg,i,true)
+    bubble(received_msg,1,true)
 }
 
-ws.onclose = ()=>{ 
+ws.onclose = () => { 
     alert("连接已关闭..."); 
 };
-
-
-bubble("111111",0,true);
-bubble("2222222",0,true);
-bubble("3333333",0,false);
 console.log(getDOM("typing"));
-
-export{sendmsg}
