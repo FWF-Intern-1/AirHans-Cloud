4c4bb5cbfab287dc2d5781bc2d236be84e5c8b59
AirboZH <993372253@qq.com>
2021-03-05 12:35:10 +0800

    自己和自己聊天

--- lab/server.js
+++ lab/server.js
@@ -1,11 +1,9 @@
-const { Socket } = require('dgram')
 const ws = require('nodejs-websocket');
 
 const server = ws.createServer(socket=>{
     socket.on('text',str=>{
         console.log(str);
-        
-        socket.sendText('收到消息了');
+        socket.sendText(str);
     })
 }).listen(9999,()=>{
     console.log('server on port 9999');
