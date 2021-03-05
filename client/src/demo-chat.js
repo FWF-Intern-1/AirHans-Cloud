af05a945a3a2883080b29c46f48a7d65c378193b
BlCatG <Bl_Cat@outlook.com>
2021-03-05 22:59:09 +0800

    sendmsg bug

--- client/src/demo-chat.js
+++ client/src/demo-chat.js
@@ -5,23 +5,29 @@ var ws = new WebSocket("ws://tomzhang.com.cn:9999");
 ws.onopen=()=>{
     console.log("connected")
 }
-function sendmsg(){
-    ws.send(getDOM("typing"))
-}
+
+// function sendmsg(){
+//     ws.send(getDOM("typing"))
+// }
 
 ws.onmessage = (evt)=>{
+    //var received_msg = getDOM("typing").value;
     var received_msg = evt.data;
-    bubble(received_msg,i,true)
+
+    console.log(typeof evt.data);
+    bubble(received_msg,1,true)
 }
 
 ws.onclose = ()=>{ 
     alert("连接已关闭..."); 
 };
 
-
 bubble("111111",0,true);
 bubble("2222222",0,true);
 bubble("3333333",0,false);
-console.log(getDOM("typing"));
 
-export{sendmsg}
+
+$("#chatBox--input--button__send").on("click",() => {
+    console.log("点击！");
+    ws.send(getDOM("typing"));
+})
