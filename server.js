a34b9ee2ef3826cb66ed9d694df5b5f15c6a34b5
AirboZH <993372253@qq.com>
2021-03-06 13:50:04 +0800

    服务器响应所有客户端

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const ws = require('nodejs-websocket');
 
-const server = ws.createServer(socket=>{
-    socket.on('text',msg=>{
-        console.log(msg);
-        socket.sendText(msg);
+
+const server = ws.createServer(socket => {
+
+    socket.on('text',conn=>{
+        msg = conn;
+        console.log(conn)
+        server.connections.forEach(socket => {
+            socket.sendText(conn)
+      })
     })
+    
 }).listen(9999,()=>{
     console.log('server on port 9999');
 });
