58e164f6edf62de12a2376ea49bd038d6828c622
AirboZH <993372253@qq.com>
2021-03-06 11:25:56 +0800

    合并

--- client/src/demo-chat.js
+++ client/src/demo-chat.js
@@ -1,12 +1,11 @@
 import { bubble } from './bubble.js'
 import { getDOM } from './getDOM.js'
 
-var ws = new WebSocket("ws://tomzhang.com.cn:9999");
+var ws = new WebSocket("ws://127.0.0.1:9999");
 
 ws.onopen=()=>{
     console.log("connected")
 }
-<<<<<<< HEAD
 
 $("#chatBox--input--button__send").on("click",() => {
     console.log("点击！");
@@ -17,35 +16,10 @@ $("#chatBox--input--button__send").on("click",() => {
 
 ws.onmessage = (evt) => {
     var received_msg = evt.data;
-=======
-
-// function sendmsg(){
-//     ws.send(getDOM("typing"))
-// }
-
-ws.onmessage = (evt)=>{
-    //var received_msg = getDOM("typing").value;
-    var received_msg = evt.data;
-
-    console.log(typeof evt.data);
->>>>>>> 11e92b05aa95aefd6e81b0af6e42bd1eb7606cc4
     bubble(received_msg,1,true)
 }
 
 ws.onclose = () => { 
     alert("连接已关闭..."); 
 };
-<<<<<<< HEAD
 console.log(getDOM("typing"));
-=======
-
-bubble("111111",0,true);
-bubble("2222222",0,true);
-bubble("3333333",0,false);
-
-
-$("#chatBox--input--button__send").on("click",() => {
-    console.log("点击！");
-    ws.send(getDOM("typing"));
-})
->>>>>>> 11e92b05aa95aefd6e81b0af6e42bd1eb7606cc4
