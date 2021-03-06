1b6a64a05c596eba6937f13678da4c2654bf603d
BlCatG <Bl_Cat@outlook.com>
2021-03-06 11:16:46 +0800

    fix bug in keyboard.js

--- client/src/keyboard.js
+++ client/src/keyboard.js
@@ -6,13 +6,15 @@ const initKeyboard = ()=> {
     $(document).keydown((event) => {
         //console.log(event.keyCode);
         if (event.keyCode == 13 && event.shiftKey) {
-            console.log("shift+enter");
+           // console.log("shift+enter");
         } else
         if (event.keyCode == 13) {
             event.preventDefault();
-            console.log("enter");
-            bubble(getDOM("typing").value,1,0);
-            getDOM("typing").value = "";
+            //console.log("enter");
+            if (getDOM("typing").value !== "") {
+                bubble(getDOM("typing").value,1,0)
+                getDOM("typing").value = "";
+            }
         }
     
     });
