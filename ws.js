d0b2898af269de8620325f887a48681d851018c3
AirboZH <993372253@qq.com>
2021-03-10 01:57:02 +0800

    增加了上线信息 优化了消息id的显示和输入

--- ws.js
+++ ws.js
@@ -11,7 +11,15 @@ const server = ws.createServer(socket => {
             socket.sendText(conn)
       })
     })
-    
+    // socket.on('close',(code,err)=>{
+    //     console.log(code,err)
+    //     var unonlineid = {
+    //         "id" : "system_information_unonline_id",     //这么长的名字应该不会真的有人会用这个id吧
+    //         "text" : 123
+    //     }
+    // })
+
 }).listen(9999,()=>{
     console.log('server on port 9999');
 });
+    
